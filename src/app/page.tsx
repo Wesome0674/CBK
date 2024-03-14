76c371b0d897f2180d9945d10c04fc4c32b83f79
Killian.bld <105119247+Wesome0674@users.noreply.github.com>
2024-03-14 18:09:06 +0100

    form brut done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,14 @@ import { Skills } from "@/ui/components/skills/Skills";
 import { Work } from "@/ui/components/work/Work";
 import { Contact } from "@/ui/components/contact/Contact";
 import { Footer } from "@/ui/components/footer/Footer";
-import { Button } from "@/ui/designsystem/Button";
-import { BsChatTextFill } from "react-icons/bs";
-import Image from "next/image";
+import QuickMessage from "@/ui/components/quickMessage/QuickMessage";
+
 
 export default function Home() {
+
   return (
     <>
-      <div className="fixed z-20 bottom-6 right-6 text-white border-2 border-secondary border-dashed rounded-full p-0.5" >
-        <Button variant="icon" size="large" icon={{ icon: BsChatTextFill }} />
-      </div>
-
-
-
+      <QuickMessage />
       <Navigation />
       <Header />
       <About />
@@ -25,6 +20,7 @@ export default function Home() {
       <Work />
       <Contact />
       <Footer />
+
     </>
   );
 }
---
+++ src/ui/components/quickMessage/QuickMessage.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Typographie } from "@/ui/designsystem/typography/Typographie"
+import React, { useState } from 'react'
+import { BsChatTextFill } from "react-icons/bs";
+import { Button } from '@/ui/designsystem/Button';
+import { IoMdClose } from "react-icons/io";
+import { Cards } from '../cards/Cards';
+
+const QuickMessage = () => {
+    const [show, setShow] = useState(false)
+
+    return (
+        <>
+            {show ?
+                <div className=" fixed z-30 bottom-6 right-6 w-[30vw]">
+                    <Cards variant="medium" className="flex flex-col shadow-md">
+                        <div >
+                            <Typographie variant="h3" className="font-semi-bold flex items-center justify-between">
+                                Message Me
+                                <IoMdClose className="cursor-pointer" onClick={() => setShow(!show)} />
+                            </Typographie>
+                            <Typographie variant="body-sm" theme="tercery">
+                                you can reach me anytime
+                            </Typographie>
+                        </div>
+                        <div className="flex flex-col space-y-3 my-6 ">
+                            <div className="flex flex-col space-y-1">
+                                <Typographie variant="body-sm" theme="tercery" >
+                                    Name
+                                </Typographie>
+                                <input className=" bg-tercery/10 text-sm font-normal p-3 rounded-[4px]" type="text" placeholder="Enter you're name" />
+                            </div>
+                            <div className="flex flex-col space-y-1">
+                                <Typographie variant="body-sm" theme="tercery" >
+                                    Email *
+                                </Typographie>
+                                <input className=" bg-tercery/10 text-sm font-normal p-3 rounded-[4px]" type="text" placeholder="mail@mail.com" />
+                            </div>
+                            <div className="flex flex-col space-y-1">
+                                <Typographie variant="body-sm" theme="tercery" >
+                                    Message
+                                </Typographie>
+                                <textarea rows={5} className=" bg-tercery/10 text-sm font-normal p-3 rounded-[4px]" placeholder="You’re message goes here" />
+                            </div>
+                        </div>
+                        <Button>Send Message</Button>
+                    </Cards>
+
+                </div>
+                :
+                <div onClick={() => setShow(!show)} className="fixed z-20 bottom-6 right-6 text-white border-2 border-secondary border-dashed rounded-full p-0.5" >
+                    <Button variant="icon" size="large" icon={{ icon: BsChatTextFill }} />
+                </div>
+            }
+        </>
+    )
+}
+
+export default QuickMessage
