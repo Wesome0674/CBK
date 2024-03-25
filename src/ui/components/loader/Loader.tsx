1c4c475ec54b947a4643f38d9d43e1bfd203a83e
Killian.bld <105119247+Wesome0674@users.noreply.github.com>
2024-03-25 19:13:18 +0100

    loader finished 100%

--- src/ui/components/loader/Loader.tsx
+++ src/ui/components/loader/Loader.tsx
@@ -9,33 +9,35 @@ const Loader = () => {
   const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
+    window.addEventListener("load", function () {
+      setIsLoading(true)
+    })
     setIsLoading(true)
     setTimeout(() => {
       setIsLoading(false)
     }, 8000)
-    
 
   }, [])
 
   return (
     <>
-      {isLoading ? 
-      <div className="fixed top-0 left-0 bg-black z-50 w-screen h-full flex justify-center items-center">
-        <div className='flex flex-col items-center'>
-          <Typographie className="text-valorant" variant="display" theme="tercery">
-            Code By Killian
-          </Typographie>
-          <Typographie variant='h3'>
-            <Typewriter
-              options={{
-                strings: ['Front-end developper', 'Designer', "passionate student"],
-                autoStart: true,
-                loop: true,
-              }}
-            />
-          </Typographie>
-        </div>
-      </div> : ""}
+      {isLoading ?
+        <div className="fixed top-0 left-0 bg-black z-50 w-screen h-full flex justify-center items-center">
+          <div className='flex flex-col items-center'>
+            <Typographie className="text-valorant" variant="display" theme="tercery">
+              Code By Killian
+            </Typographie>
+            <Typographie variant='h3'>
+              <Typewriter
+                options={{
+                  strings: ['Front-end developper', 'Designer', "passionate student"],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </Typographie>
+          </div>
+        </div> : ""}
     </>
   );
 };
